2af7536828a0a18524cbfddfff37a1373120d715
Ian Stewart <ianstewart4outlook.com>
2021-02-21 10:32:38 -0600

    completed game and added personal touch of "remaining tries"

--- guessingGame.js
+++ guessingGame.js
@@ -2,9 +2,27 @@
 let maximum = parseInt(prompt("Enter a maximum number"));
 
 //While user has not entered a valid number it asks to enter a valid number
-while(!maximum){
+while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"))
 }
 
+//Creates a randomly generated number below the max to be guessed
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
+
+//Prompts user to guess a number and stores it
+let guess = parseInt(prompt("Guess a number"));
+
+//Amount of guesses user gets
+let guessCount = 5;
+
+//While guess is wrong asks user again and reduces number of remaining guesses
+while (guess !== targetNum && guessCount > 0) {
+    guess = parseInt(prompt(`Wrong number, guess again! \nYou have ${guessCount} tries left`))
+    guessCount--;
+    if (guess === targetNum) {
+        console.log("YOU GOT IT RIGHT");
+        break;
+    }
+}
+
