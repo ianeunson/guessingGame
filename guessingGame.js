f9ad365276f7e363d1906ba0c12d1b3236cb7eb1
Ian Stewart <ianstewart4outlook.com>
2021-02-21 07:49:02 -0600

    added target number to be guessed

--- guessingGame.js
+++ guessingGame.js
@@ -5,3 +5,6 @@ let maximum = parseInt(prompt("Enter a maximum number"));
 while(!maximum){
     maximum = parseInt(prompt("Enter a valid number!"))
 }
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
